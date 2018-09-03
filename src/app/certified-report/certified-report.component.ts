91e47c5b6fd3970726249013092bfd42cc3dfb49
sharadks <sharadeduworld@gmail.com>
2018-09-03 13:33:26 +0530

    Resolved

--- src/app/certified-report/certified-report.component.ts
+++ src/app/certified-report/certified-report.component.ts
@@ -9,13 +9,13 @@ import {ChartModule} from 'primeng/chart';
 export class CertifiedReportComponent implements OnInit {
 
 public data: any;
-public pieColors : any:
+public pieColors : any;
 
   constructor() {
   this.pieColors = ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099',
 '#3B3EAC', '#0099C6', '#DD4477', '#66AA00', '#B82E2E',
 '#316395', '#994499', '#22AA99', '#AAAA11', '#6633CC',
-'#E67300', '#8B0707', '#329262', '#5574A6', '#3B3EAC']
+'#E67300', '#8B0707', '#329262', '#5574A6', '#3B3EAC'];
 		this.data = {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
